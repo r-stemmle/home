94e359a97e946fb51ecb819cc0d8e502e18544b6
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-17 12:41:58 -0500

    Add methods to building Iter3

--- lib/building.rb
+++ lib/building.rb
@@ -3,12 +3,35 @@ class Building
 
   def initialize
     @units = []
+    @rented_units = []
+  end
+
+  def units_by_number_of_bedrooms
+    units_hash = Hash.new { |hash, rooms| hash[rooms] = [] }
+    units.each do |unit|
+      units_hash[unit.bedrooms] << unit.number
+    end
+    units_hash
+  end
+
+  def renter_with_highest_rent
+    highest = rented_units[-1].renter
+    rented_units.sort_by do |unit|
+      unit.monthly_rent
+    end
+    highest = rented_units[-1].renter
   end
 
   def average_rent
     rent_array = @units.map { |unit| unit.monthly_rent }
     total_amount = rent_array.inject(0) { |sum, rent| sum + rent }
     total_amount.to_f / @units.length
+  end
+
+  def rented_units
+    @units.find_all do |unit|
+      unit.renter != nil
+    end
   end
 
   def renters
