14dcd27a278636f1f7d9d034c866a1591e69d248
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-17 11:26:47 -0500

    Pass all apartment tests

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -4,4 +4,41 @@ require './lib/apartment'
 require './lib/renter'
 
 class ApartmentTest < Minitest::Test
+  def setup
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
+
+  def test_it_exists
+    @unit1
+
+    assert_instance_of Apartment, @unit1
+  end
+
+  def test_it_has_apt_number
+    @renter1
+    @unit1
+
+    assert_equal "A1", @unit1.number
+  end
+
+  def test_it_has_monthly_rent
+
+    assert_equal 1200, @unit1.monthly_rent
+  end
+
+  def test_it_has_bathrooms
+
+    assert_equal 1, @unit1.bathrooms
+  end
+
+  def test_it_has_bedrooms
+
+    assert_equal 1, @unit1.bedrooms
+  end
+
+  def test_it_has_not_a_renter_by_default
+
+    assert_equal nil, @unit1.renter
+  end
+
 end
