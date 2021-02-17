36b13a8ff5bcfead3fea7d2ee6731c32074a0965
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-17 12:41:21 -0500

    Pass all building class tests

--- test/building_test.rb
+++ test/building_test.rb
@@ -7,10 +7,13 @@ require './lib/renter'
 class BuildingTest < Minitest::Test
   def setup
     @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
     @building = Building.new
-    @renter1 = Renter.new("Aurora")
-    @renter2 = Renter.new("Tim")
+    @renter1 = Renter.new("Spencer")
+    @renter2 = Renter.new("Jessie")
+    @renter3 = Renter.new("Max")
   end
 
   def test_it_exists
@@ -43,14 +46,57 @@ class BuildingTest < Minitest::Test
     @unit1.add_renter(@renter1)
     @unit2.add_renter(@renter2)
 
-    assert_equal ["Aurora", "Tim"], @building.renters
+    assert_equal ["Spencer", "Jessie"], @building.renters
   end
 
   def test_it_can_find_average_rent
     @building.add_unit(@unit1)
     @building.add_unit(@unit2)
-    
+
     assert_equal 1099.5, @building.average_rent
+  end
+
+  def test_has_no_rented_units_by_default
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+
+    assert_equal [], @building.rented_units
+  end
+
+  def test_it_can_have_rented_units
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    @unit2.add_renter(@renter1)
+
+    assert_equal [@unit2], @building.rented_units
+  end
+
+  def test_it_can_find_renter_with_highest_rent
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    @unit2.add_renter(@renter1)
+    @unit3.add_renter(@renter3)
+
+    assert_equal @renter3, @building.renter_with_highest_rent
+  end
+
+  def test_it_can_arrange_units_by_number_of_bedrooms
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    @unit2.add_renter(@renter1)
+    @unit3.add_renter(@renter3)
+    @building.add_unit(@unit4)
+
+    expected = { 3 => ["D4"],
+                 2 => ["B2", "C3"],
+                 1 => ["A1"]
+               }
+
+    assert_equal expected, @building.units_by_number_of_bedrooms
   end
 
 end
