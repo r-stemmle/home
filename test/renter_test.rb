2d264f27913a66e0005bde3200a7b2a56dbcc9bb
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-17 11:19:15 -0500

    Pass renter_test

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -3,13 +3,20 @@ require 'minitest/pride'
 require './lib/renter'
 
 class RenterTest < Minitest::Test
+  def setup
+    @renter1 = Renter.new("Jessie")
+    # @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
   def test_it_exists
     jessie = Renter.new("Jessie")
+
     assert_instance_of Renter, jessie
   end
 
   def test_it_has_a_name
     jessie = Renter.new("Jessie")
+
     assert_equal "Jessie", jessie.name
   end
+
 end
